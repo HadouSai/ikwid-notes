0053556891d14b033321fa8c86157882805624ba
Alexander Guatibonza <diegosferas@gmail.com>
2021-04-06 23:07:29 +0000

    Correciones

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/HadouSai/ikwid-notes
 go 1.16
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/rs/cors v1.7.0 // indirect
-	go.mongodb.org/mongo-driver v1.5.1 // indirect
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.7.0
+	go.mongodb.org/mongo-driver v1.5.1
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
 )
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -12,7 +12,7 @@ import (
 	"github.com/HadouSai/ikwid-notes/routers"
 )
 
-/* Handler Sett the port and ListenAndServe with mux */
+// Handlers :  Sett the port and ListenAndServe with mux
 func Handlers() {
 	router := mux.NewRouter() // capture http and hangle w/r, if has information to send a response
 
@@ -27,6 +27,10 @@ func Handlers() {
 
 	handler := cors.AllowAll().Handler(router) // cors take the control
 
-	log.Fatal(http.ListenAndServe(":"+PORT, handler))
+	//log.Fatal(http.ListenAndServe(":"+PORT, handler))
+
+	if err := http.ListenAndServe(":"+PORT, handler); err != nil {
+		log.New(os.Stdout, "[Error]", 0).Fatalf("Error: No se pudo iniciar el servidor: 127.0.0.1:%s. \n Message: %v", PORT, err)
+	}
 
 }
